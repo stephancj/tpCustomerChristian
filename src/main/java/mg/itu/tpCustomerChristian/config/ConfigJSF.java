2d0a058669f4c35fc206c1a77a2f9f6f70a25f98
Stéphan J. CHRISTIAN <christianstephanjosue@gmail.com>
2023-03-13 20:31:44 +0300

    Ajout d'une DataTable JSF dans la page customerList

---
+++ src/main/java/mg/itu/tpCustomerChristian/config/ConfigJSF.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpCustomerChristian.config;
+
+/**
+ *
+ * @author Stéphan J. Christian
+ */
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
---
+++ src/main/java/mg/itu/tpCustomerChristian/jsf/CustomerBean.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+
+/**
+ *
+ * @author mcfmacbookpro2
+ * mg.itu.tpCustomerChristian
+ */
+package mg.itu.tpCustomerChristian.jsf;
+        
+import jakarta.ejb.EJB;  
+import jakarta.inject.Named;  
+import jakarta.faces.view.ViewScoped;  
+import java.io.Serializable;  
+import java.util.List;  
+import mg.itu.tpCustomerChistian.entities.Customer;
+
+import mg.itu.tpCustomerChristian.ejb.CustomerManager;
+
+/**
+ * Backing bean de la page customerList.xhtml.
+ */
+@Named(value = "customerBean")  
+@ViewScoped  
+public class CustomerBean implements Serializable {  
+  private List<Customer> customerList;  
+
+  @EJB
+  private CustomerManager customerManager;  
+        
+  public CustomerBean() {  }  
+        
+  /** 
+   * Retourne la liste des clients pour affichage dans une DataTable.
+   */  
+  public List<Customer> getCustomers() {
+    if (customerList == null) {
+      customerList = customerManager.getAllCustomers();
+    }
+    return customerList;
+  }  
+}
+
